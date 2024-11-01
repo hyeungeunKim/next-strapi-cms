2a0fc26c1c2d8152a6673aeedc8fd2561ffd36a1
hyeongeunKim <goon126@ncsoft.com>
2024-11-01 10:35:01 +0900

    fix: Update next.config.js for Strapi image domains and remote patterns

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -2,19 +2,36 @@
 
 const nextConfig = {
   images: {
+    domains: [
+      process.env.NEXT_PUBLIC_STRAPI_DOMAIN ||
+        'thankful-flowers-70ac5271d4.media.strapiapp.com',
+      'media.strapiapp.com'
+    ],
     remotePatterns: [
       {
         protocol: 'http',
         hostname: 'localhost',
         port: '1337',
-        pathname: '/uploads/**',
+        pathname: '/uploads/**'
       },
       {
         protocol: 'https',
-        hostname: 'images.pexels.com',
+        hostname: 'images.pexels.com'
+      },
+      {
+        protocol: 'https',
+        hostname: '*.media.strapiapp.com',
+        port: '',
+        pathname: '/**'
+      },
+      {
+        protocol: 'https',
+        hostname: 'thankful-flowers-70ac5271d4.media.strapiapp.com',
+        port: '',
+        pathname: '/**'
       }
-    ],
-  },
+    ]
+  }
 }
 
 module.exports = nextConfig
